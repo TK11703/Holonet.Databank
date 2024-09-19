eff15837b8f2d2de53d6ff78b91ebb099557f957
TK11703 <tk11703@outlook.com>
2024-09-19 16:14:39 -0400

    Update program structure

--- Holonet.Databank.API/Program.cs
+++ Holonet.Databank.API/Program.cs
@@ -1,53 +1,45 @@
+using Holonet.Databank.API;
 
-namespace Holonet.Databank.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthorization();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
-			// Add services to the container.
-			builder.Services.AddAuthorization();
+app.UseHttpsRedirection();
 
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+app.UseAuthorization();
 
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseAuthorization();
-
-			var summaries = new[]
-			{
+var summaries = new[]
+{
 				"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 			};
 
-			app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-			{
-				var forecast = Enumerable.Range(1, 5).Select(index =>
-					new WeatherForecast
-					{
-						Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-						TemperatureC = Random.Shared.Next(-20, 55),
-						Summary = summaries[Random.Shared.Next(summaries.Length)]
-					})
-					.ToArray();
-				return forecast;
-			})
-			.WithName("GetWeatherForecast")
-			.WithOpenApi();
+app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+{
+	var forecast = Enumerable.Range(1, 5).Select(index =>
+		new WeatherForecast
+		{
+			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = summaries[Random.Shared.Next(summaries.Length)]
+		})
+		.ToArray();
+	return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
 
-			app.Run();
-		}
-	}
-}
+app.Run();
