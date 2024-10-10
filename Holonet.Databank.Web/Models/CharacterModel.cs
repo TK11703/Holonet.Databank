53dbcc3a46b039b209990e003469873cece7f57c
TK11703 <tk11703@outlook.com>
2024-10-10 16:52:01 -0400

    Altered the character form to enable the breakdown of birthdate into two fields.

--- Holonet.Databank.Web/Models/CharacterModel.cs
+++ Holonet.Databank.Web/Models/CharacterModel.cs
@@ -6,21 +6,49 @@ public class CharacterModel
 {
 	public int Id { get; set; }
 
-	[Required]
-	[StringLength(150)]
+	[Required(ErrorMessage ="Given name is required.")]
+	[StringLength(150, ErrorMessage = "Given name can only be {1} characters in length.")]
 	public string GivenName { get; set; } = string.Empty;
 
-	[StringLength(150)]
+	[StringLength(150, ErrorMessage = "Family name can only be {1} characters in length.")]
 	public string? FamilyName { get; set; } = string.Empty;
 
 	public string? Description { get; set; }
 
 	[Url]
-	[StringLength(500)]
+	[StringLength(500, ErrorMessage = "Shard can only be {1} characters in length.")]
 	public string? Shard { get; set; }
 
 	[StringLength(200)]
-	public string? BirthDate { get; set; }
+	public string? BirthDate
+	{
+		get
+		{
+			return BirthYear.HasValue && !string.IsNullOrEmpty(DateSystem) ? $"{BirthYear} {DateSystem}" : null;
+		}
+		set
+		{
+			if (string.IsNullOrEmpty(value))
+			{
+				BirthYear = null;
+				DateSystem = null;
+			}
+			else
+			{
+				var parts = value.Split(' ', 2);
+				if (parts.Length == 2)
+				{
+					BirthYear = int.Parse(parts[0]);
+					DateSystem = parts[1];
+				}
+
+			}
+		}
+	}
+
+	public int? BirthYear { get; set; }
+
+	public string? DateSystem { get; set; }
 
 	public int? PlanetId { get; set; }
 
