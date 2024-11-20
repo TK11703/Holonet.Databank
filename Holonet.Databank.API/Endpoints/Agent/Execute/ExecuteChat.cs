b667de0831e8413f24cfd321ce490c57c7b5b83d
TK11703 <tk11703@outlook.com>
2024-11-20 11:29:55 -0500

    Applying updates to chat components and adding icons for agent interaction.
    This reverts commit e709464e76b1318e0568003611602685ead2a4ca.

--- Holonet.Databank.API/Endpoints/Agent/Execute/ExecuteChat.cs
+++ Holonet.Databank.API/Endpoints/Agent/Execute/ExecuteChat.cs
@@ -1,15 +1,10 @@
 ﻿using Holonet.Databank.API.Filters;
 using Holonet.Databank.Application.AICapabilities;
-using Holonet.Databank.Application.AICapabilities.Plugins;
-using Holonet.Databank.Application.Services;
 using Holonet.Databank.Core.Dtos;
-using Holonet.Databank.Core.Entities;
 using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-using System;
 
 namespace Holonet.Databank.API.Endpoints.Agent.Execute;
 
@@ -26,26 +21,13 @@ public class ExecuteChat : IEndpoint
 		try
 		{
 			ArgumentNullException.ThrowIfNull(chatRequest);
-			//var azureSearchExtensionConfiguration = new AzureSearchChatExtensionConfiguration
-			//{
-			//	SearchEndpoint = new Uri(Environment.GetEnvironmentVariable("AZURE_AI_SEARCH_ENDPOINT")),
-			//	Authentication = new OnYourDataApiKeyAuthenticationOptions(Environment.GetEnvironmentVariable("AZURE_AI_SEARCH_API_KEY")),
-			//	IndexName = Environment.GetEnvironmentVariable("AZURE_AI_SEARCH_INDEX")
-			//};
 
-			//var chatExtensionsOptions = new AzureChatExtensionsOptions { Extensions = { azureSearchExtensionConfiguration } };
-			//var executionSettings = new OpenAIPromptExecutionSettings { AzureChatExtensionsOptions = chatExtensionsOptions };
+            var settings = new OpenAIPromptExecutionSettings { Temperature = 0.8, TopP = 0.0, ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
 
-			//var result = await kernel.InvokePromptAsync("What are my available health plans?", new(executionSettings));
-
-			var chatHistory = chatHistoryManager.GetOrCreateChatHistory(chatRequest.AzureId.ToString());
-
-			//kernel.ImportPluginFromObject(new DBQueryPlugin(_configuration));
+            var chatHistory = chatHistoryManager.GetOrCreateChatHistory(chatRequest.AzureId.ToString());            
 			chatHistory.AddUserMessage(chatRequest.Prompt);
 
-			ChatMessageContent? result = await chat.GetChatMessageContentAsync(chatHistory,
-				  executionSettings: new OpenAIPromptExecutionSettings { Temperature = 0.8, TopP = 0.0, ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions },
-				  kernel: kernel);
+			ChatMessageContent? result = await chat.GetChatMessageContentAsync(chatHistory, executionSettings: settings, kernel: kernel);
 
             if (result == null || string.IsNullOrEmpty(result.Content))
             {
