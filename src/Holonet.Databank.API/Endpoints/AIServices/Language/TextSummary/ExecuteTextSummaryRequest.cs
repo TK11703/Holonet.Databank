31e7a145db601e918f768ae39172168874d05771
TK11703 <tk11703@outlook.com>
2025-07-10 16:15:36 -0400

    #17

--- src/Holonet.Databank.API/Endpoints/AIServices/Language/TextSummary/ExecuteTextSummaryRequest.cs
+++ src/Holonet.Databank.API/Endpoints/AIServices/Language/TextSummary/ExecuteTextSummaryRequest.cs
@@ -1,7 +1,10 @@
-﻿using Holonet.Databank.API.Filters;
+﻿using Holonet.Databank.API.Configuration;
+using Holonet.Databank.API.Filters;
 using Holonet.Databank.Application.Services.AI;
 using Holonet.Databank.Core.Entities;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Holonet.Databank.API.Endpoints.AIServices.Language.TextSummary;
 
@@ -13,15 +16,14 @@ public class ExecuteTextSummaryRequest : IEndpoint
             .AddEndpointFilter<ValidatorFilter<TextSummaryRequest>>()
             .WithTags(Tags.AIServices);
     }
-    protected virtual async Task<Results<Ok<TextSummaryResult>, ProblemHttpResult>> HandleAsync(TextSummaryRequest request, IConfiguration config)
+    protected virtual async Task<Results<Ok<TextSummaryResult>, ProblemHttpResult>> HandleAsync(TextSummaryRequest request, [FromServices] IOptions<AppSettings> settings)
     {
+        AppSettings appSettings = settings.Value;
         TextSummaryResult result = new TextSummaryResult();
         try
         {
-            SummarizationService service = new SummarizationService(config.GetValue<string>("AzureOpenAi:Model")!, config.GetValue<string>("AzureOpenAi:Endpoint")!, config.GetValue<string>("AzureOpenAi:ApiKey")!);
+            SummarizationService service = new SummarizationService(appSettings.AzureOpenAi.Model!, appSettings.AzureOpenAi.Endpoint!, appSettings.AzureOpenAi.ApiKey!);
             result.ResultText = await service.SummarizeContentAsync(request.Input);
-            //TextAnalysisService service = new TextAnalysisService(config.GetValue<string>("AzureAiLanguage:ApiKey")!, config.GetValue<string>("AzureAiLanguage:Endpoint")!);
-            //result.ResultText = await service.GetSummaryAbstract(request.Input);
             return TypedResults.Ok(result);
         }
         catch (Exception ex)
